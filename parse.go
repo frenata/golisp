1c5da04553574d667a3b6413f24ba0ccc0861a48
Andrew Nichols <danweasel@gmail.com>
2017-04-18 12:04:18 +0700

    Test for whitespace stripping

--- parse.go
+++ parse.go
@@ -16,16 +16,27 @@ func NewToken(token string) *lisp {
 
 func ParseLisp(str string) (*lisp, error) {
 	l := &lisp{}
-
 	l.list = make([]*lisp, 0)
+	str = strings.TrimSpace(str)
 
 	token := ""
+	addToken := func() {
+		if token != "" {
+			l.list = append(l.list, NewToken(token))
+			token = ""
+		}
+	}
+
 	for i := 1; i < len(str); i++ {
 		c := str[i : i+1]
 		switch c {
 		case " ":
-			l.list = append(l.list, NewToken(token))
-			token = ""
+			addToken()
+		case ")":
+			addToken()
+			return l, nil
+		default:
+			token += c
 		case "(":
 			match := strings.Index(str[i:], ")")
 			nest, err := ParseLisp(str[i : i+match+1])
@@ -34,11 +45,6 @@ func ParseLisp(str string) (*lisp, error) {
 			}
 			l.list = append(l.list, nest)
 			i = i + match + 1
-		case ")":
-			l.list = append(l.list, NewToken(token))
-			return l, nil
-		default:
-			token += c
 		}
 	}
 	return nil, errors.New("no ')' found")
--- parse_test.go
+++ parse_test.go
@@ -1,7 +1,38 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestParens(t *testing.T) {
+	badParens := []string{"(+ 1", ")(", "((((((", "(()", "((()()()))", ""}
 
+	for _, s := range badParens {
+		res, err := ParseLisp(s)
+		t.Log(res, err)
+		if err == nil {
+			t.Fatalf("Parsing %s did not produce an error", s)
+		}
+	}
+}
+
+func TestInternalWhitespace(t *testing.T) {
+	input := "(   5   6     9      )"
+	expected := "(5 6 9)"
+	actual, _ := ParseLisp(input)
+
+	if fmt.Sprint(actual) != expected {
+		t.Fatal("whitespace is not properly stripped from %s when parsing, %s should be %s", input, actual, expected)
+	}
+}
+
+func TestExternalWhitespace(t *testing.T) {
+	input := "   (   5   6     9      )    "
+	expected := "(5 6 9)"
+	actual, _ := ParseLisp(input)
+
+	if fmt.Sprint(actual) != expected {
+		t.Fatalf("whitespace is not properly stripped from %s when parsing, %s should be %s", input, actual, expected)
+	}
 }
